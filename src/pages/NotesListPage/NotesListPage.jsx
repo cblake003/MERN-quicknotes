511e114020b84d222d56c2b4f5989c949c3195c1
cblake003 <cameronburtner@gmail.com>
2023-10-09 16:54:21 -0700

    added new notes to note list page

--- src/pages/NotesListPage/NotesListPage.jsx
+++ src/pages/NotesListPage/NotesListPage.jsx
@@ -1,20 +1,23 @@
 import NoteCard from "../../components/NoteCard/NoteCard";
 import * as notesAPI from "../../utilities/notes-api"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import NewNotesPage from "../../pages/NewNotesPage/NewNotesPage"
 
 export default function NotesListPage({ notes, setNotes }) {
-    
+    const [newNotes, setNewNotes] = useState([])
+
     useEffect(function () {
         (async function() {
             const allNotes = await notesAPI.getAll()
             setNotes([...allNotes])
         })()
-    }, [])
+    }, [newNotes])
 
     const notesList = notes.map((note) => <NoteCard note={note} />)
     
     return(
         <>
+        <NewNotesPage setNewNotes={setNewNotes} />
         {notes.length ? notesList : "No Notes Yet!"}
         </>
     );
